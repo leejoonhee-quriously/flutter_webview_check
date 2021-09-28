aae09c6cd1472067b64da7ebd792c3020cf99b5e
leejoonhee-quriously <joonhee@quriously.com>
2021-09-28 19:04:48 +0900

    Implement library webview_flutter

--- aikrlass_webview_check/lib/page/login_page.dart
+++ aikrlass_webview_check/lib/page/login_page.dart
@@ -1,13 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: WebView(
-          initialUrl: 'https://www.google.com',
+          initialUrl: 'https://www.airklass.com',
           javascriptMode: JavascriptMode.unrestricted,
         ),
       ),
