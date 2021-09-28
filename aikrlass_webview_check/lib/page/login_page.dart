9e884993bbee79e791b142d16b83ffb59153abc6
leejoonhee-quriously <joonhee@quriously.com>
2021-09-28 15:50:42 +0900

    Add webview page

--- aikrlass_webview_check/lib/page/login_page.dart
+++ aikrlass_webview_check/lib/page/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'https://www.google.com',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
   }
 }
